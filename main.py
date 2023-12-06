0038090abdab01aaea947fb09fe630d2425b7619
Marianopopov <mariano.popov@gmail.com>
2023-12-06 13:06:01 -0300

    model, API

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def UserForGenre(genero):
             'Uruario con mas horas jugadas por genero': usuario_agrupado_1 [0],
             'Horas Jugadas': poranio
         }
-
+        
         return dicc
     
     except Exception as e:
@@ -47,15 +47,51 @@ def UserForGenre(genero):
 
 
 @app.get('/UsersRecommend/ {año}')
-def UsersRecommend(year: str):
-    try:   
+
+def UsersRecommend(year: int):
+    try:
         function3 = pd.read_csv('./Datasets/function3.csv')
         filtered_df = function3[(function3['recommend'] == 1) & (function3['sentiment_analysis'].isin([1, 2])) & (function3['year'] == year)]
         game_recommendations = filtered_df.groupby('app_name')['recommend'].count().sort_values(ascending=False)
-        top_3_games = game_recommendations.head(3)
-        result = [{"Puesto {}: {}".format(i + 1, juego)} for i, juego in enumerate(top_3_games)]
-        
+        top_3_games = game_recommendations.head(3).to_dict()
+        result = {"year": year, "top_3_games": [{"puesto {}".format(i + 1): {game}} for i, (game, _) in enumerate(top_3_games.items())]}
         return result
 
     except Exception as e:
         return {"Error": str(e)}
+
+
+
+@app.get('/UsersNotRecommend/ {año}')
+
+def UsersNotRecommend(year: int):
+    try:
+        function3 = pd.read_csv('./Datasets/function3.csv')
+        filtered_df = function3[(function3['recommend'] == 0) & (function3['sentiment_analysis'].isin([0, -1])) & (function3['year'] == year)]
+        game_not_recommendations = filtered_df.groupby('app_name')['recommend'].count().sort_values(ascending=False)
+        top_3_not_recommended = game_not_recommendations.head(3).to_dict()
+        result = {"year": year, "top_3_not_recommended": [{"puesto {}".format(i + 1): {game}} for i, (game, _) in enumerate(top_3_not_recommended.items())]}
+        return result
+
+    except Exception as e:
+        return {"Error": str(e)}
+    
+    
+    
+@app.get('/sentiment_analysis/ {año}')
+def sentiment_analysis(year: int):
+    try:
+        function3 = pd.read_csv('./Datasets/function3.csv.gz', compression='gzip')
+        filtered_df = function3[function3['year'] == year]
+        sentiment_counts = filtered_df.groupby('sentiment_analysis').size().reset_index(name='count')
+        result = {
+            'Negative': int(sentiment_counts.loc[sentiment_counts['sentiment_analysis'] == 0, 'count'].values[0]) if 0 in sentiment_counts['sentiment_analysis'].values else 0,
+            'Neutral': int(sentiment_counts.loc[sentiment_counts['sentiment_analysis'] == 1, 'count'].values[0]) if 1 in sentiment_counts['sentiment_analysis'].values else 0,
+            'Positive': int(sentiment_counts.loc[sentiment_counts['sentiment_analysis'] == 2, 'count'].values[0]) if 2 in sentiment_counts['sentiment_analysis'].values else 0
+        }
+
+        return result
+
+    except Exception as e:
+        return {"Error": str(e)}
+
