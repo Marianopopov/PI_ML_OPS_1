6177499941e81dfb80cdae91b6e588540509f335
Marianopopov <mariano.popov@gmail.com>
2023-12-13 16:44:00 -0300

    clean_files

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def bienvenida():
 
 def PlayTimeGenre( genero : str ):
     try:
-        consulta_final = pd.read_csv('result_df.csv.gz')
+        consulta_final = pd.read_csv('./Datasets/merged/result_df.csv.gz')
         # Filtrar el DataFrame por el género especificado
         genero_df = consulta_final[consulta_final['genres'] == genero]
 
@@ -42,7 +42,7 @@ def PlayTimeGenre( genero : str ):
 
 def UserForGenre(genre):
     try:
-            consulta_final = pd.read_csv('02_nombres_max.csv.gz', index_col=['index'])
+            consulta_final = pd.read_csv('./Datasets/merged/02_nombres_max.csv.gz', index_col=['index'])
             user_max = consulta_final.loc[genre].nombre
             diccionario = ast.literal_eval(consulta_final.loc[genre].year)
             diccionario['Horas_Jugadas'] = diccionario.pop('playtime_forever')
@@ -58,7 +58,7 @@ def UserForGenre(genre):
 
 def UsersRecommend(year: int):
     try:
-        function3 = pd.read_csv('./Datasets/function3.csv.gz', compression='gzip')
+        function3 = pd.read_csv('./Datasets/merged/function3.csv.gz', compression='gzip')
         filtered_df = function3[(function3['recommend'] == 1) & (function3['sentiment_analysis'].isin([1, 2])) & (function3['year'] == year)]
         game_recommendations = filtered_df.groupby('app_name')['recommend'].count().sort_values(ascending=False)
         top_3_games = game_recommendations.head(3).to_dict()
@@ -74,7 +74,7 @@ def UsersRecommend(year: int):
 
 def UsersNotRecommend(year: int):
     try:
-        function3 = pd.read_csv('./Datasets/function3.csv.gz', compression='gzip')
+        function3 = pd.read_csv('./Datasets/merged/function3.csv.gz', compression='gzip')
         filtered_df = function3[(function3['recommend'] == 0) & (function3['sentiment_analysis'].isin([0, -1])) & (function3['year'] == year)]
         game_not_recommendations = filtered_df.groupby('app_name')['recommend'].count().sort_values(ascending=False)
         top_3_not_recommended = game_not_recommendations.head(3).to_dict()
@@ -89,7 +89,7 @@ def UsersNotRecommend(year: int):
 @app.get('/sentiment_analysis/ {año}')
 def sentiment_analysis(year: int):
     try:
-        function3 = pd.read_csv('./Datasets/function3.csv.gz', compression='gzip')
+        function3 = pd.read_csv('./Datasets/merged/function3.csv.gz', compression='gzip')
         filtered_df = function3[function3['year'] == year]
         sentiment_counts = filtered_df.groupby('sentiment_analysis').size().reset_index(name='count')
         result = {
@@ -107,7 +107,7 @@ def sentiment_analysis(year: int):
 @app.get('/recomendacion_juego/ {app_name}')
 def recomendacion_juego_v2(item_id :int):
     try:
-        consulta_06 = pd.read_csv('recomendacion_juego.csv.gz',compression='gzip')
+        consulta_06 = pd.read_csv('./Datasets/merged/recomendacion_juego.csv.gz',compression='gzip')
         
         nombre_juego = consulta_06.set_index('item_id').loc[item_id].values[0].split(',')[0]
 
